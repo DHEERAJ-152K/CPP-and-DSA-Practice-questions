382a3a23d4ddfec779cfc42ad311c10617bc5f8b
Dheeraj K P <dheerudheeraj033@gmail.com>
2022-12-08 01:34:06 +0530

    Update allDivisor.cpp

--- Math/allDivisor.cpp
+++ Math/allDivisor.cpp
@@ -4,8 +4,8 @@ void printdivisors(int num){
     int divisors=1;
    for (int i = 1; i <=num; i++)
    {
-    if(num%i==0)
-        cout<<i<<"\t";
+    if(num%i==0)        //check the num is equals to 0 or not.
+        cout<<i<<"\t"; 
    }
 }
 int main(){
