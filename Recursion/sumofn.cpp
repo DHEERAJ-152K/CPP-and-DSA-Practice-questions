167647e493e4a758ec1ff86550aca3ae50798f87
Dheeraj K P <dheerudheeraj033@gmail.com>
2022-12-09 23:04:25 +0530

    Add files via upload

---
+++ Recursion/sumofn.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sumofn(int n){
+    if(n==0)
+        return 0;
+    return n+sumofn(n-1);
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<sumofn(n);
+    return 0;
+    
+}
